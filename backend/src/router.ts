75c3a019eb1870e8bd66fdd8e72038e37bf050f3
Nicolás Lopez Vidueiros <139637937+Nico235711@users.noreply.github.com>
2024-08-28 15:02:23 -0300

    Creando una función para crear productos

--- backend/src/router.ts
+++ backend/src/router.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express'
+import { createProduct } from './handlers/product'
 
 const router = Router()
 
 // routing
+router.post("/", createProduct)
+
 router.get("/", (req, res) => {
   res.send("hola mundo")
 })
