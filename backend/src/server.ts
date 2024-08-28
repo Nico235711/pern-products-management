51fb9ef5a10d01776d9c1915cf6ab36cdb815eb7
Nicolás Lopez Vidueiros <139637937+Nico235711@users.noreply.github.com>
2024-08-28 12:51:26 -0300

    Colocando la conexión a la BD en variables de Entorno

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,18 @@
-import express from 'express'
-import router from './router'
+import express from 'express';
+import router from './router';
+import { db } from './config/db';
+
+// conectar a la base de datos
+async function connectDB() {
+  try {
+    await db.authenticate();
+    db.sync()
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+}
+connectDB()
 
 // creando el servidor
 const server = express()
