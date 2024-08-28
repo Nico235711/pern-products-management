81674b3e45e3f73c61f23ed02c61b28acf28f206
Nicolás Lopez Vidueiros <139637937+Nico235711@users.noreply.github.com>
2024-08-28 15:41:55 -0300

    Moviendo la validación hacía el Router

--- backend/src/handlers/product.ts
+++ backend/src/handlers/product.ts
@@ -2,6 +2,12 @@ import { Request, Response } from "express"
 import Product from "../models/Products.model"
 
 export const createProduct = async (req: Request, res: Response) => {
-  const product = await Product.create(req.body)
-  res.json(product)
+  try {
+    const product = await Product.create(req.body)
+    res.json({ data: product })
+
+  } catch (error) {
+    console.log(error);
+
+  }
 }
--- backend/src/router.ts
+++ backend/src/router.ts
@@ -1,10 +1,21 @@
 import { Router } from 'express'
 import { createProduct } from './handlers/product'
+import { body } from 'express-validator'
+import { handleInputErrors } from './middleware/handleInputErrors'
 
 const router = Router()
 
 // routing
-router.post("/", createProduct)
+router.post("/", 
+  body("name")
+    .notEmpty().withMessage("El nombre del producto no puede ir vacío"),
+  body("price")
+    .notEmpty().withMessage("El precio no puede ir vacío")
+    .isNumeric().withMessage("El precio debe ser número")
+    .custom(value => value > 0).withMessage("Precio no válido"),
+  handleInputErrors,
+  createProduct
+)
 
 router.get("/", (req, res) => {
   res.send("hola mundo")
