b53d2bfe671493389fdf9edba2cbc81b413a4ffc
Nico235711 <139637937+Nico235711@users.noreply.github.com>
2025-01-28 17:16:14 -0300

    agregando tipado

--- frontend/src/components/Error.tsx
+++ frontend/src/components/Error.tsx
@@ -1,5 +1,6 @@
+import { PropsWithChildren } from "react";
 
-export default function Error({ children }) {
+export default function Error({ children }: PropsWithChildren) {
 
   return (
     <p className="text-red-600 text-center mb-5 text-lg font-bold">{children}</p>
