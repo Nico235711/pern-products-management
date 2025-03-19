e87519631ce1b2fd25ecae5fb5aaeffee530d57f
Nico235711 <139637937+Nico235711@users.noreply.github.com>
2025-03-19 19:20:39 -0300

    agregando validación

--- server/src/routes/productsRoutes.ts
+++ server/src/routes/productsRoutes.ts
@@ -1,16 +1,41 @@
 import { Router } from "express";
 import { ProductsController } from "../controllers/products";
+import { body, param } from "express-validator";
+import { errorHandler } from "../middleware";
 
 const router = Router()
 
-router.post("/", ProductsController.createProduct)
+router.post("/", 
+  body("name")
+    .notEmpty().withMessage("El nombre del producto no puede ir vacío"),
+  body("price")
+    .isNumeric().withMessage("El precio del producto no es válido")
+    .custom(value => value > 0).withMessage("El precio del producto debe ser mayor a cero"),
+  errorHandler,
+  ProductsController.createProduct
+)
 
 router.get("/", ProductsController.getAllProducts)
 
-router.get("/:id", ProductsController.getProductById)
+router.get("/:id", 
+  param("id")
+    .isNumeric().withMessage("ID no válido"),
+  errorHandler,
+  ProductsController.getProductById
+)
 
-router.put("/:id", ProductsController.updateProductById)
+router.put("/:id", 
+  param("id")
+    .isNumeric().withMessage("ID no válido"),
+  errorHandler,
+  ProductsController.updateProductById
+)
 
-router.delete("/:id", ProductsController.deleteProductById)
+router.delete("/:id", 
+  param("id")
+    .isNumeric().withMessage("ID no válido"),
+  errorHandler,
+  ProductsController.deleteProductById
+)
 
 export default router
